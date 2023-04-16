cd6a73b14ab1c21c6309144a0aad6a0f34e04a58
Pekka Enberg <penberg@iki.fi>
2023-04-16 13:30:31 +0300

    Drop mutex from Client

--- src/client.rs
+++ src/client.rs
@@ -3,8 +3,9 @@ use crate::message::Message;
 use crate::types::{ClientID, ReplicaID, RequestNumber, ViewNumber};
 use crossbeam_channel::Sender;
 use log::trace;
-use parking_lot::Mutex;
+use std::cell::RefCell;
 use std::fmt::Debug;
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 
 pub type ClientCallback = Box<dyn Fn(RequestNumber) + Send>;
@@ -18,12 +19,8 @@ where
     client_id: ClientID,
     view_number: ViewNumber,
     replica_tx: Sender<(ReplicaID, Message<Op>)>,
-    inner: Mutex<ClientInner>,
-}
-
-struct ClientInner {
-    request_number: RequestNumber,
-    callbacks: Option<(RequestNumber, ClientCallback)>,
+    request_number: AtomicUsize,
+    callbacks: RefCell<Option<(RequestNumber, ClientCallback)>>,
 }
 
 impl<Op> Client<Op>
@@ -31,29 +28,23 @@ where
     Op: Clone + Debug + Send,
 {
     pub fn new(config: Arc<Config>, replica_tx: Sender<(ReplicaID, Message<Op>)>) -> Client<Op> {
-        let request_number = 0;
-        let callbacks = None;
-        let inner = ClientInner {
-            request_number,
-            callbacks,
-        };
-        let inner = Mutex::new(inner);
+        let request_number = AtomicUsize::new(0);
+        let callbacks = RefCell::new(None);
         Client {
             config,
             client_id: 0,
             view_number: 0,
             replica_tx,
-            inner,
+            request_number,
+            callbacks,
         }
     }
 
     pub fn on_request(&self, op: Op, callback: ClientCallback) {
         trace!("Client {} <- {:?}", self.client_id, op);
         let primary_id = self.config.primary_id(self.view_number);
-        let mut inner = self.inner.lock();
-        let request_number = inner.request_number;
-        inner.request_number += 1;
-        inner.callbacks.replace((request_number, callback));
+        let request_number = self.request_number.fetch_add(1, Ordering::SeqCst);
+        self.callbacks.replace(Some((request_number, callback)));
         self.replica_tx
             .send((
                 primary_id,
@@ -67,8 +58,8 @@ where
     }
 
     pub fn on_message(&self) {
-        let mut inner = self.inner.lock();
-        if let Some((request_number, callback)) = inner.callbacks.take() {
+        let mut callbacks = self.callbacks.borrow_mut();
+        if let Some((request_number, callback)) = callbacks.take() {
             callback(request_number);
         }
     }
