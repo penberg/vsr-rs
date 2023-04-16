e4d01cf495e94b539fe91cd9c836c8753ce17c4f
Pekka Enberg <penberg@iki.fi>
2023-04-16 13:47:29 +0300

    Drop mutex from Replica

--- src/replica.rs
+++ src/replica.rs
@@ -3,9 +3,10 @@ use crate::message::Message;
 use crate::types::{CommitID, OpNumber, ReplicaID, ViewNumber};
 use crossbeam_channel::Sender;
 use log::trace;
-use parking_lot::Mutex;
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::fmt::Debug;
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 
 /// Replica status.
@@ -31,26 +32,16 @@ where
 {
     config: Arc<Config>,
     self_id: ReplicaID,
-    inner: Mutex<ReplicaInner<S, Op>>,
+    state_machine: Arc<S>,
+    status: RefCell<Status>,
+    view_number: ViewNumber,
+    commit_number: AtomicUsize,
+    op_number: AtomicUsize,
+    log: RefCell<Vec<Op>>,
+    acks: RefCell<HashMap<ReplicaID, usize>>,
     client_tx: Sender<()>,
     replica_tx: Sender<(ReplicaID, Message<Op>)>,
 }
-
-#[derive(Debug)]
-struct ReplicaInner<S, Op>
-where
-    Op: Clone + Debug + Send,
-{
-    state_machine: Arc<S>,
-    status: Status,
-    view_number: ViewNumber,
-    commit_number: CommitID,
-    op_number: OpNumber,
-    log: Vec<Op>,
-    acks: HashMap<ReplicaID, usize>,
-}
-
-impl<S, Op> ReplicaInner<S, Op> where Op: Clone + Debug + Send {}
 
 impl<S, Op> Replica<S, Op>
 where
@@ -64,13 +55,15 @@ where
         client_tx: Sender<()>,
         replica_tx: Sender<(ReplicaID, Message<Op>)>,
     ) -> Replica<S, Op> {
-        let status = Status::Normal;
+        let status = RefCell::new(Status::Normal);
         let view_number = 0;
-        let commit_number = 0;
-        let op_number = 0;
-        let log = Vec::default();
-        let acks = HashMap::default();
-        let inner = ReplicaInner {
+        let commit_number = AtomicUsize::new(0);
+        let op_number = AtomicUsize::new(0);
+        let log = RefCell::new(Vec::default());
+        let acks = RefCell::new(HashMap::default());
+        Replica {
+            self_id,
+            config,
             state_machine,
             status,
             view_number,
@@ -78,25 +71,18 @@ where
             op_number,
             log,
             acks,
-        };
-        let inner = Mutex::new(inner);
-        Replica {
-            self_id,
-            config,
             client_tx,
             replica_tx,
-            inner,
         }
     }
 
     pub fn on_idle(&self) {
-        let inner = self.inner.lock();
-        if !self.is_primary(&inner) {
+        if !self.is_primary() {
             return;
         }
-        assert_eq!(inner.status, Status::Normal);
-        let view_number = inner.view_number;
-        let commit_number = inner.commit_number;
+        assert_eq!(*self.status.borrow(), Status::Normal);
+        let view_number = self.view_number;
+        let commit_number = self.commit_number();
         self.send_msg_to_others(Message::Commit {
             view_number,
             commit_number,
@@ -155,17 +141,17 @@ where
     }
 
     fn on_request(&self, op: Op) {
-        let mut inner = self.inner.lock();
         // TODO: If not primary, drop request, advise client to connect to primary.
-        assert!(self.is_primary(&inner));
+        assert!(self.is_primary());
         // TODO: If not in normal status, drop request, advise client to try later.
-        assert_eq!(inner.status, Status::Normal);
-        self.append_to_log(&mut inner, op.clone());
-        let op_number = inner.op_number;
-        inner.acks.insert(op_number, 1);
+        assert_eq!(*self.status.borrow(), Status::Normal);
+        self.append_to_log(op.clone());
+        let op_number = self.op_number();
+        let mut acks = self.acks.borrow_mut();
+        acks.insert(op_number, 1);
         // TODO: Update client_table
-        let view_number = inner.view_number;
-        let commit_number = inner.commit_number;
+        let view_number = self.view_number;
+        let commit_number = self.commit_number();
         self.send_msg_to_others(Message::Prepare {
             view_number,
             op,
@@ -186,29 +172,28 @@ where
         op_number: OpNumber,
         commit_number: CommitID,
     ) {
-        let mut inner = self.inner.lock();
-        assert!(!self.is_primary(&inner));
+        assert!(!self.is_primary());
         // TODO: If view number is not the same, initiate recovery.
-        assert_eq!(inner.view_number, view_number);
+        assert_eq!(self.view_number, view_number);
         // If we fell behind in the log, initiate state transfer.
-        if op_number > inner.op_number + 1 {
-            self.state_transfer(&mut inner);
+        if op_number > self.op_number() + 1 {
+            self.state_transfer();
             return;
         }
-        if op_number <= inner.op_number {
+        if op_number <= self.op_number() {
             return; // duplicate
         }
-        assert_eq!(inner.op_number + 1, op_number);
+        assert_eq!(self.op_number() + 1, op_number);
         // Append op to our log.
-        self.append_to_log(&mut inner, op);
+        self.append_to_log(op);
         // Commit the log up to the commit number received in `Prepare`
         // message, which represents the committed state of the primary.
-        for op_idx in inner.commit_number..commit_number {
-            self.commit_op(&mut inner, op_idx);
+        for op_idx in self.commit_number()..commit_number {
+            self.commit_op(op_idx);
         }
         // Acknowledge the `Prepare` message to the primary.
-        let primary_id = self.primary_id(&inner);
-        let view_number = inner.view_number;
+        let primary_id = self.primary_id();
+        let view_number = self.view_number;
         self.send_msg(
             primary_id,
             Message::PrepareOk {
@@ -223,51 +208,50 @@ where
     // received `PrepareOk` messages from a quorum of replicas, it commits
     // the operation and replies to the client.
     fn on_prepare_ok(&self, view_number: ViewNumber, op_number: OpNumber) {
-        let mut inner = self.inner.lock();
-        assert!(self.is_primary(&inner));
-        assert_eq!(inner.view_number, view_number);
+        assert!(self.is_primary());
+        assert_eq!(self.view_number, view_number);
         // Register the acknowledgement
-        let acks = inner.acks.get_mut(&op_number).unwrap();
+        let mut acks = self.acks.borrow_mut();
+        let acks = acks.get_mut(&op_number).unwrap();
         *acks += 1;
         // If we have received a quorum of `PrepareOk` messages, commit the
         // operation and reply to the client.
         if *acks == self.config.quorum() {
-            self.commit_op(&mut inner, op_number - 1);
+            self.commit_op(op_number - 1);
             self.respond_to_client();
         }
     }
 
     fn on_commit(&self, view_number: ViewNumber, commit_number: CommitID) {
-        let mut inner = self.inner.lock();
-        if inner.status != Status::Normal {
+        if *self.status.borrow() != Status::Normal {
             return;
         }
-        if view_number < inner.view_number {
+        if view_number < self.view_number {
             return;
         }
-        assert_eq!(inner.status, Status::Normal);
-        assert_eq!(inner.view_number, view_number);
-        if commit_number > inner.op_number {
-            self.state_transfer(&mut inner);
+        assert_eq!(*self.status.borrow(), Status::Normal);
+        assert_eq!(self.view_number, view_number);
+        if commit_number > self.op_number.load(Ordering::SeqCst) {
+            self.state_transfer();
             return;
         }
-        for op_idx in inner.commit_number..commit_number {
-            self.commit_op(&mut inner, op_idx);
+        for op_idx in self.commit_number()..commit_number {
+            self.commit_op(op_idx);
         }
     }
 
     fn on_get_state(&self, replica_id: ReplicaID, view_number: ViewNumber, op_number: OpNumber) {
-        let inner = self.inner.lock();
-        assert_eq!(inner.status, Status::Normal);
-        assert_eq!(inner.view_number, view_number);
+        assert_eq!(*self.status.borrow(), Status::Normal);
+        assert_eq!(self.view_number, view_number);
+        let log = self.log.borrow();
         self.send_msg(
             replica_id,
             Message::NewState {
-                view_number: inner.view_number,
-                log: inner.log[op_number..].to_vec(),
+                view_number: self.view_number,
+                log: log[op_number..].to_vec(),
                 op_number_start: op_number,
-                op_number_end: inner.op_number,
-                commit_number: inner.commit_number,
+                op_number_end: self.op_number(),
+                commit_number: self.commit_number(),
             },
         );
     }
@@ -280,24 +264,23 @@ where
         op_number_end: OpNumber,
         commit_number: CommitID,
     ) {
-        let mut inner = self.inner.lock();
-        if inner.status != Status::Recovery {
+        if *self.status.borrow() != Status::Recovery {
             return;
         }
-        assert_eq!(inner.status, Status::Recovery);
-        assert_eq!(inner.view_number, view_number);
-        assert_eq!(op_number_start, inner.op_number);
+        assert_eq!(*self.status.borrow(), Status::Recovery);
+        assert_eq!(self.view_number, view_number);
+        assert_eq!(op_number_start, self.op_number());
         for op in log {
-            self.append_to_log(&mut inner, op);
+            self.append_to_log(op);
         }
-        for op_idx in inner.commit_number..commit_number {
-            self.commit_op(&mut inner, op_idx);
+        for op_idx in self.commit_number()..commit_number {
+            self.commit_op(op_idx);
         }
-        assert_eq!(inner.op_number, op_number_end);
-        assert_eq!(inner.commit_number, commit_number);
-        inner.status = Status::Normal;
-        let view_number = inner.view_number;
-        let primary_id = self.primary_id(&inner);
+        assert_eq!(self.op_number(), op_number_end);
+        assert_eq!(self.commit_number(), commit_number);
+        self.status.replace(Status::Normal);
+        let view_number = self.view_number;
+        let primary_id = self.primary_id();
         self.send_msg(
             primary_id,
             Message::PrepareOk {
@@ -307,30 +290,32 @@ where
         );
     }
 
-    fn append_to_log(&self, inner: &mut ReplicaInner<S, Op>, op: Op) {
-        inner.log.push(op);
-        inner.op_number += 1;
+    fn append_to_log(&self, op: Op) {
+        let mut log = self.log.borrow_mut();
+        log.push(op);
+        self.op_number.fetch_add(1, Ordering::SeqCst);
     }
 
-    fn state_transfer(&self, inner: &mut ReplicaInner<S, Op>) {
-        inner.status = Status::Recovery;
+    fn state_transfer(&self) {
+        self.status.replace(Status::Recovery);
         // FIXME: pick *one* replica, doesn't need to be primary.
-        let primary_id = self.primary_id(inner);
+        let primary_id = self.primary_id();
         self.send_msg(
             primary_id,
             Message::GetState {
                 replica_id: self.self_id,
-                view_number: inner.view_number,
-                op_number: inner.op_number,
+                view_number: self.view_number,
+                op_number: self.op_number(),
             },
         );
     }
 
     /// Commits an operation at log index `op_idx`.
-    fn commit_op(&self, inner: &mut ReplicaInner<S, Op>, op_idx: usize) {
-        let op = &inner.log[op_idx];
-        inner.state_machine.apply(op.clone());
-        inner.commit_number += 1;
+    fn commit_op(&self, op_idx: usize) {
+        let log = self.log.borrow();
+        let op = &log[op_idx];
+        self.state_machine.apply(op.clone());
+        self.commit_number.fetch_add(1, Ordering::SeqCst);
     }
 
     /// Sends a message to all other replicas.
@@ -352,11 +337,19 @@ where
         self.client_tx.send(()).unwrap();
     }
 
-    fn is_primary(&self, inner: &ReplicaInner<S, Op>) -> bool {
-        self.self_id == self.primary_id(inner)
+    fn is_primary(&self) -> bool {
+        self.self_id == self.primary_id()
     }
 
-    fn primary_id(&self, inner: &ReplicaInner<S, Op>) -> ReplicaID {
-        self.config.primary_id(inner.view_number)
+    fn primary_id(&self) -> ReplicaID {
+        self.config.primary_id(self.view_number)
+    }
+
+    fn commit_number(&self) -> CommitID {
+        self.commit_number.load(Ordering::SeqCst)
+    }
+
+    fn op_number(&self) -> OpNumber {
+        self.op_number.load(Ordering::SeqCst)
     }
 }
